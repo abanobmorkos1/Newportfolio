0493a25e4831be4f93386baea2d8d133a3dbe971
abanobmorkos1 <abanobmorkos11@gmail.com>
2024-02-12 02:34:02 -0500

    finishing touches

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,18 +2,18 @@ import Lottie from "lottie-react";
 import Contacts from '../contacts.json'
 
 function Footer () {
-    return <div className="footer">
+    return <div id="footer">
         <div className="contactani">
         <Lottie
         animationData={Contacts}
         />        
         </div>
 
-        <div className='contactinfo'>
+        <div id='contactinfo'>
         <h1>Let's connect</h1>
         <p>AbanobMorkos11@gmail.com</p>
-        <p><a href=''>GitHub</a></p>
-        <p>LinkedIn</p>
+        <p><a href='https://github.com/abanobmorkos1'>GitHub</a></p>
+        <p><a href='https://linkedin.com/in/abanob-morkos'>LinkedIn</a></p>
         </div>
         </div>
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,11 @@
-import Resume from '../resumefol/NewResume.pdf'
+import Resume from '../resumefol/2024.pdf'
 function navBar(){
     return (
             <nav className="nav">
                 <ul className="ul_nav">
                     <li className='project'><a href="#projectcontainer">Projects</a></li>
                     <li className='skillsnav'><a href={Resume} target="_blank" rel="noreferrer">Resume</a></li>
-                    <li className='contacts'><a href="#footer">Contact</a></li>
+                    <li className='contacts'><a href="#contactinfo">Contact</a></li>
                 </ul>
             </nav>
         );
